0efbf6cf112f18c98bcc908316ee5f5f054078fc
Jean Andreghetto <jean377@gmail.com>
2017-11-20 12:25:56 -0200

    attr_write

--- lib/sidekiq-scheduler/web.rb
+++ lib/sidekiq-scheduler/web.rb
@@ -6,32 +6,28 @@ module SidekiqScheduler
   # Hook into *Sidekiq::Web* Sinatra app which adds a new '/recurring-jobs' page
 
   module Web
-    VIEW_PATH = File.expand_path('../../../web/views', __FILE__)
-
-    class << self
-      def view_path
-        @view_path ||= VIEW_PATH
-      end
-      attr_writer :view_path
-
-      def registered(app)
-        app.get '/recurring-jobs' do
-          @presented_jobs = JobPresenter.build_collection(Sidekiq.schedule!)
-          erb File.read(File.join(VIEW_PATH, 'recurring_jobs.erb'))
-        end
-        app.get '/recurring-jobs/:name/enqueue' do
-          schedule = Sidekiq.get_schedule(params[:name])
-          SidekiqScheduler::Scheduler.enqueue_job(schedule)
-          redirect "#{root_path}recurring-jobs"
-        end
-        app.get '/recurring-jobs/:name/toggle' do
-          Sidekiq.reload_schedule!
-          SidekiqScheduler::Scheduler.toggle_job_enabled(params[:name])
-          redirect "#{root_path}recurring-jobs"
-        end
-      end
+    unless Dir.exist? "#{Rails.root}/app/views/sidekiq"
+      VIEW_PATH = File.expand_path('../../../web/views', __FILE__)
+    else
+      VIEW_PATH = "#{Rails.root}/app/views/sidekiq/"
     end
 
+    def self.registered(app)
+      app.get '/recurring-jobs' do
+        @presented_jobs = JobPresenter.build_collection(Sidekiq.schedule!)
+        erb File.read(File.join(VIEW_PATH, 'recurring_jobs.erb'))
+      end
+      app.get '/recurring-jobs/:name/enqueue' do
+        schedule = Sidekiq.get_schedule(params[:name])
+        SidekiqScheduler::Scheduler.enqueue_job(schedule)
+        redirect "#{root_path}recurring-jobs"
+      end
+      app.get '/recurring-jobs/:name/toggle' do
+        Sidekiq.reload_schedule!
+        SidekiqScheduler::Scheduler.toggle_job_enabled(params[:name])
+        redirect "#{root_path}recurring-jobs"
+      end
+    end
     
   end
 end
